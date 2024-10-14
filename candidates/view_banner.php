76ed5ce386c4208dea03dc0709d048a780dc3fa8
abelle30 <141491818+abelle30@users.noreply.github.com>
2024-10-15 00:44:27 +0800

    Add files via upload

---
+++ candidates/view_banner.php
@@ -0,0 +1,18 @@
+
+<div class="side-bar" 
+            style="background-color:#002F6C; font-size:25px; text-align:center; margin-top:-10px; padding-top:20px;">
+
+                <a style="color:white; font-weight:bold; font-size:30px;">2022 Student Government Candidates</a>
+                </a>
+
+<nav class="nav-menue">
+            <ul>
+                <li><a href="../index.php">Home</a></li>
+                <li><a href="../view.php">Candidates</a></li>
+                <li><a href="../about.php">About AdU</a></li>
+                <li><a href="../register/index.php">Register</a></li>
+            
+            </ul>
+</nav>
+					
+</div>
